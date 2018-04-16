97413986ab46d2d101bf94878416fc117044a8fb
Josselin Auguste <jauguste@iblop.net>
2018-04-16 22:35:00 +0200

    Append transaction

--- src/Blockchain.hs
+++ src/Blockchain.hs
@@ -1,1 +1,39 @@
-module Blockchain where
+module Blockchain
+  ( appendTransaction
+  , makeBlockchain
+  , Transaction(..)
+  , Blockchain(blocks, currentTransactions)
+  ) where
+
+import Data.Time.Clock (UTCTime)
+
+data Proof =
+  Proof
+
+data Hash =
+  Hash
+
+data Transaction =
+  Transaction
+  deriving (Eq, Show)
+
+data Block = Block
+  { index :: Int
+  , timestamp :: UTCTime
+  , transactions :: [Transaction]
+  , proof :: Proof
+  , previousHash :: Hash
+  }
+
+data Blockchain = Blockchain
+  { blocks :: [Block]
+  , currentTransactions :: [Transaction]
+  }
+
+makeBlockchain :: Blockchain
+makeBlockchain = Blockchain {blocks = [], currentTransactions = []}
+
+appendTransaction :: Transaction -> Blockchain -> Blockchain
+appendTransaction transaction blockchain =
+  blockchain
+  {currentTransactions = transaction : currentTransactions blockchain}
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Control.Exception (evaluate)
+import Test.Hspec
+
+import Blockchain
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $
+  describe "append transaction to the chain" $
+  it "returns the new blockchain with the transaction" $ do
+    let updatedBlockchain = appendTransaction transaction blockchain
+    head (currentTransactions updatedBlockchain) `shouldBe` transaction
+  where
+    transaction = Transaction
+    blockchain = makeBlockchain
