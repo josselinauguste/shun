17506ee7aca8e47320f61453c31bcb205658ca6f
Josselin Auguste <jauguste@iblop.net>
2018-04-19 21:30:32 +0200

    Parameterize hash constraint to allow the test to pass in an acceptable time

--- src/Proof.hs
+++ src/Proof.hs
@@ -1,11 +1,12 @@
 module Proof
   ( proofOfWork
+  , defaultHashConstraint
   , Proof
   , Hash(Hash)
   ) where
 
 import Crypto.Hash.SHA256 (hash)
-import Data.ByteString (isPrefixOf)
+import Data.ByteString (ByteString, isPrefixOf)
 import Data.ByteString.Char8 (pack)
 import Data.List (find)
 
@@ -17,20 +18,25 @@ data Hash =
   Hash
   deriving (Eq, Show)
 
-proofOfWork :: Maybe Proof -> Maybe Proof
-proofOfWork Nothing = computeProofOfWork genesisProof
-proofOfWork (Just previousProof) = computeProofOfWork previousProof
+type HashConstraint = ByteString -> Bool
+
+proofOfWork :: HashConstraint -> Maybe Proof -> Maybe Proof
+proofOfWork constraint Nothing = computeProofOfWork constraint genesisProof
+proofOfWork constraint (Just previousProof) =
+  computeProofOfWork constraint previousProof
 
 genesisProof :: Proof
 genesisProof = Proof 100
 
-computeProofOfWork :: Proof -> Maybe Proof
-computeProofOfWork previousProof =
-  find (validProof previousProof) potentialProofs
+computeProofOfWork :: HashConstraint -> Proof -> Maybe Proof
+computeProofOfWork constraint previousProof =
+  find (validProof constraint previousProof) potentialProofs
   where
     potentialProofs = [Proof i | i <- [1 ..]]
 
-validProof :: Proof -> Proof -> Bool
-validProof (Proof previousProof) (Proof proofToValidate) =
-  pack "0000" `isPrefixOf`
-  hash (pack $ show previousProof ++ show proofToValidate)
+validProof :: HashConstraint -> Proof -> Proof -> Bool
+validProof constraint (Proof previousProof) (Proof proofToValidate) =
+  constraint (hash $ pack $ show previousProof ++ show proofToValidate)
+
+defaultHashConstraint :: HashConstraint
+defaultHashConstraint = isPrefixOf (pack "0000")
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,5 @@
-import Control.Exception (evaluate)
+import Data.ByteString (isPrefixOf)
+import Data.ByteString.Char8 (pack)
 import qualified Data.List.NonEmpty as NonEmpty
 import Data.Time.Clock (getCurrentTime)
 import Test.Hspec
@@ -9,6 +10,7 @@ import Proof
 main :: IO ()
 main =
   hspec $ do
+    let simpleHashConstraint = isPrefixOf (pack "0")
     describe "append transaction to the chain" $
       it "returns the new blockchain with the transaction" $ do
         let transaction = Transaction
@@ -16,18 +18,21 @@ main =
         let updatedBlockchain = appendTransaction transaction blockchain
         head (currentTransactions updatedBlockchain) `shouldBe` transaction
     describe "proof of work" $
-      it "computes proof for the first block" $ do
+      it "computes proof for the first block with a very low hash constraint" $ do
         let blockchain = makeBlockchain
-        show (proofOfWork $ lastProof blockchain) `shouldBe` ""
+        show (proofOfWork simpleHashConstraint $ lastProof blockchain) `shouldBe`
+          "Just (Proof 172)"
     describe "create a block" $
       it "returns the validated block and reinitialize transactions" $ do
         let blockchain = makeBlockchain
-        let (Just proof) = proofOfWork $ lastProof blockchain
-        timestamp <- getCurrentTime
-        let updatedBlockchain = createBlock timestamp proof blockchain
+        let (Just expectedProof) =
+              proofOfWork simpleHashConstraint $ lastProof blockchain
+        expectedTimestamp <- getCurrentTime
+        let updatedBlockchain =
+              createBlock expectedTimestamp expectedProof blockchain
         length (blocks updatedBlockchain) `shouldBe` (2 :: Int)
         let block = NonEmpty.head $ blocks updatedBlockchain
         index block `shouldBe` (2 :: Int)
-        Blockchain.timestamp block `shouldBe` timestamp
+        timestamp block `shouldBe` expectedTimestamp
         transactions block `shouldBe` []
-        Blockchain.proof block `shouldBe` proof
+        proof block `shouldBe` expectedProof
